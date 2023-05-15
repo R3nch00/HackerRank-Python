2d80049244fd4a5a3fcd2555e82b802d794fc4eb
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-05-16 01:18:13 +0600

    Add files via upload

---
+++ Capitalize!.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+def solve(s):
+    ans=s.split(' ') # Split method to split input and declared it as and
+    ans1=(((i.capitalize() for i in ans)))
+    return' '.join(ans1)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = solve(s)
+    fptr.write(result + '\n')
+    fptr.close()
