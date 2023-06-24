03c1b4618195272d4c9d5227ea2639b2f1c87600
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-06-24 23:43:20 +0600

    Update Zeros and Ones.py

--- Zeros and Ones.py
+++ Zeros and Ones.py
@@ -1,4 +1,9 @@
-import numpy
-nums=tuple(map(int,input().split()))
-print(numpy.zeros(nums,dtype=numpy.int))
-print(numpy.ones(nums,dtype=numpy.int)
+import numpy as np
+n=tuple(map(int,input().split()))
+a=np.zeros(n,int)
+b=np.ones(n,int)
+
+print(a)
+print(b)
+
+
